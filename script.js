d8ef5842ae01e1f9dc50d577124e5c18f7b9afc0
Florencia <flopywood@Florencias-MacBook-Pro-2.local>
2016-12-01 12:59:20 -0300

    minimizing
    minimized code for better performance

--- script.js
+++ script.js
@@ -10,8 +10,6 @@ $(document).ready(function() {
       url: wikiURL,
       dataType: "jsonp",
       success: function(data) {
-       // console.log(data);
-
         document.getElementById("wikiSearchResults").innerHTML = "";
 
         var title = data[1];
@@ -19,18 +17,13 @@ $(document).ready(function() {
         var links = data[3];
 
         for (i = 0; i < title.length; i++) {
-
           document.getElementById("wikiSearchResults").innerHTML += "<div id='row'><div class='col-md-6 col-md-offset-3'></div><a href=" + links[i]  + "><div class='result col-md-6 col-md-offset-3'><h3>" + title[i] + "</h3><blockquote>" + summaries[i] + "</blockquote></a><div class='col-md-6 col-md-offset-3'></div></div>";
         }
         console.log(title[0] + summaries[0] + links[0]);
-
-
       },
       error: function(displayErrorMessage) {
         alert("Error");
       },
     });
-
   });
-
 });
